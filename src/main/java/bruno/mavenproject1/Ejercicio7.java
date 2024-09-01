fcbfb1ee275f4401077ebf94fdef80d2b010ef9a
Bruno Freijomil <bruno_freijomil@hotmail.com>
2024-09-01 13:31:46 -0300

    Exercise 8 solved

--- src/main/java/bruno/mavenproject1/Ejercicio7.java
+++ src/main/java/bruno/mavenproject1/Ejercicio7.java
@@ -7,25 +7,74 @@ Nota: Utilizar estructuras selectivas. Tener en cuenta los años bisiestos
  */
 package bruno.mavenproject1;
 
+import static bruno.mavenproject1.Ejercicio6.esBisiestro;
 import java.time.LocalDateTime;
-
-
+import java.util.Scanner;
 
 /*
                ¡¡¡¡¡¡¡       EJRCICIO  INCOMPLETO     !!!!!!!!!!!
 */
 
-
-
 public class Ejercicio7 {
     public static void main(String[] args){
         System.out.println("------ EJERCICIO 7 ------");
         LocalDateTime fechaActual = LocalDateTime.now();
+        Scanner scanner = new Scanner(System.in);
         
-        int anio = fechaActual.getYear();
-        int mes = fechaActual.getMonthValue();
-        int dia = fechaActual.getDayOfMonth();
+        /*
+        System.out.print("Ingrese dia: ");
+        int diaIngresado = scanner.nextInt();
+        System.out.print("Ingrese mes: ");
+        int mesIngresado = scanner.nextInt();
+        */
+        System.out.print("Ingrese anio nacimiento: ");
+        int anioIngresado = scanner.nextInt();
         
-        System.out.println("Fecha actual: " + dia + "/" + mes + "/" + anio);
+        int anioActual = fechaActual.getYear();
+        int mesActual = fechaActual.getMonthValue();
+        int diaActual = fechaActual.getDayOfMonth();
+        
+        int diasVividos = 0;
+        
+       
+        // System.out.println("Fecha Ingresada: " + diaIngresado + "/" + mesIngresado + "/" + anioIngresado);
+        System.out.println("Fecha actual: " + diaActual + "/" + mesActual + "/" + anioActual);
+        
+        
+        /*
+        for(int i = 1; i < mesActual + 1; i++){
+            diasVividos += 30;
+        }
+        */
+        
+        // paso los años desde X hasta el actual en días.
+        for (int i = anioIngresado; i <= anioActual; i++) {
+            if (esBisiestro(i)) {
+                diasVividos += 366;
+            } else {
+                diasVividos += 365;
+            }
+        }
+        
+        System.out.println("Has vivido " + diasVividos + " dias.");
+        scanner.close();
     }
+    
+    
+    public static boolean esBisiestro(int anio){
+        /*
+        Si el año es divisible por 4, se verifica si también es divisible por 100. Si es así, el año podría no ser bisiesto. 
+        Si el año es divisible por 100, se verifica si también es divisible por 400. 
+        Si es divisible por 400, el año es bisiesto (true), de lo contrario, no lo es (false).
+        */
+        if (anio % 4 == 0) {
+            if (anio % 100 == 0) {
+                return anio % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    } 
 }
---
+++ src/main/java/bruno/mavenproject1/Ejercicio8.java
@@ -0,0 +1,56 @@
+/*
+[A.08] Recibos de sueldo
+Por teclado se ingresa el valor hora, el nombre, la antigüedad (en años) y la cantidad de
+horas trabajadas en el mes de N empleados de una fábrica.
+Se pide calcular el importe a cobrar teniendo en cuenta que el total (que resulta de multiplicar
+el valor hora por la cantidad de horas trabajadas), hay que sumarle la cantidad de años
+trabajados multiplicados por $51000, y al total de todas esas operaciones restarle el 13% en
+concepto de descuentos.
+Mostrar el recibo correspondiente con el nombre, la antigüedad, el valor hora, el total a cobrar
+en bruto, el total de descuentos y el valor neto a cobrar de todos los empleados ingresados.
+Nota: Utilizar estructuras repetitivas y selectivas.
+ */
+package bruno.mavenproject1;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args){
+        System.out.println("------ EJERCICIO 8 ------");
+        Scanner scanner = new Scanner(System.in);
+       
+        while (true){
+            System.out.print("Ingrese el nombre (o 'salir' para terminar): ");
+            String nombre = scanner.nextLine();
+            if (nombre.equalsIgnoreCase("salir")) {
+                break;
+            }
+            System.out.print("Ingrese el valor x hora: ");
+            int valorHora = scanner.nextInt();
+            System.out.print("Anios de antiguedad: ");
+            int aniosAntiguedad = scanner.nextInt();
+            System.out.print("Horas trabajadas en el mes: ");
+            int horasEnElMes = scanner.nextInt();
+            
+            int valorPorMes = valorHora * horasEnElMes;
+            //int valorTotal = valorPorMes * aniosAntiguedad * 12;
+            int valorTotal = valorPorMes + aniosAntiguedad * 51000;
+            double valorDescuento = valorTotal * 0.13;
+            
+            scanner.nextLine(); // Limpiar el buffer para leer una nueva entrada.
+            
+            System.out.println(" ");
+            System.out.println("******* RECIBO *******");
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Antiguedad: " + aniosAntiguedad);
+            System.out.println("Valor hora: " + valorHora);
+            System.out.println("Total bruto: " + valorTotal);
+            System.out.println("Total de descuento: " + valorDescuento);
+            System.out.println("Valor NETO: " + (valorTotal - valorDescuento));
+            System.out.println("**********************");
+            System.out.println(" ");
+            
+        }
+        scanner.close();
+    }
+}
