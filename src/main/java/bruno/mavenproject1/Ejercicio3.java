58d9df9042f551f5caa80dbef7167c403003e35c
Bruno Freijomil <bruno_freijomil@hotmail.com>
2024-09-05 00:03:55 -0300

    exersise 3 v.2

--- src/main/java/bruno/mavenproject1/Ejercicio3.java
+++ src/main/java/bruno/mavenproject1/Ejercicio3.java
@@ -6,6 +6,7 @@ Nota: Utilizar estructuras repetitivas, selectivas y la función módulo (%).
  */
 package bruno.mavenproject1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio3 {
@@ -13,18 +14,38 @@ public class Ejercicio3 {
         System.out.println("------ EJERCICIO 3 ------");
         
         Scanner numero = new Scanner(System.in);
-        System.out.print("Ingrese el numero: ");
-        int numeroIngresado = numero.nextInt();
+        boolean datoCorrecto = false;
         
-        System.out.println("Numeros primos hasta " + numeroIngresado + ":");
-        //recorrer desde el 2 hasta el num ingresado
-        for (int i = 2; i <= numeroIngresado; i++) {
-            if (esPrimo(i)) {
-                System.out.print(i + " ");
+        System.out.print("Ingrese el numero: ");
+        
+        do {
+           try {
+            int numeroIngresado = numero.nextInt();
+            
+            if (numeroIngresado <= 0) {
+                throw new IllegalArgumentException(); 
             }
-        }
+            
+            System.out.println("Numeros primos hasta " + numeroIngresado + ":");
+            //recorrer desde el 2 hasta el num ingresado
+            for (int i = 2; i <= numeroIngresado; i++) {
+                if (esPrimo(i)) {
+                    System.out.print(i + " ");
+                }
+            }
+            datoCorrecto = true;
+            } catch (InputMismatchException e) {
+                System.out.print("ERROR! Ingrese un dato correcto: ");
+                numero.next();
+            } catch (IllegalArgumentException e){
+                System.out.print("ERROR! Valor debe ser mayor a 0: ");
+            }
+        } while (!datoCorrecto);
+        
         numero.close();
     }
+    
+    
     
     public static boolean esPrimo(int num){
         if (num <= 1) {
