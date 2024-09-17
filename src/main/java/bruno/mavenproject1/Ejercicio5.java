15e2efd112814efa7231d24cc78ba9a0c322fe57
Bruno Freijomil <bruno_freijomil@hotmail.com>
2024-09-17 15:55:10 -0300

    generic commit

---
+++ src/main/java/bruno/mavenproject1/Ejercicio5.java
@@ -0,0 +1,18 @@
+/*
+[A.05] Centros Numéricos
+Un centro numérico es un número que separa una lista de números enteros (comenzando
+en 1) en dos grupos de números, cuyas sumas son iguales.
+El primer centro numérico es el 6, el cual separa la lista (1 a 8) en los grupos: (1; 2; 3; 4; 5)
+y (7; 8) cuyas sumas son ambas iguales a 15. El segundo centro numérico es el 35, el cual 
+separa la lista (1 a 49) en los grupos: (1 a 34) y (36 a 49) cuyas sumas son ambas iguales a 595.
+Se pide elaborar una aplicación que calcule los centros numéricos entre 1 y el número que
+el usuario ingres por consola.
+Nota: Utilizar estructuras repetitivas y selectivas.
+ */
+package bruno.mavenproject1;
+
+public class Ejercicio5 {
+    public static void main(String[] args){
+        System.out.println("------ EJERCICIO 5 ------");
+    }
+}
